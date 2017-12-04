4832125d5408fd768728d17a86aa9e921886957c
Benjamin <Benjamin@XPS11.home>
2017-12-04 20:41:41 +0100

    Ajout de méthodes dans "utils" + début de Treefish

--- src/utils.java
+++ src/utils.java
@@ -4,6 +4,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.math.*;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
@@ -11,7 +12,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.util.*;
 
 public class utils {
 	
@@ -53,5 +54,30 @@ public class utils {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+	
+	//Retourne une chaîne de bits dont la longueur est définie en paramètre
+	static String[] KeyGenerator(int size) {
+		String[] tab;
+        tab = new String[3];
+        
+		BigInteger k = new BigInteger((size-128), new Random());
+		BigInteger t0 = new BigInteger((64), new Random());
+		BigInteger t1 = new BigInteger((64), new Random());
+		
+		System.out.println("Clé initiale (" + size + " bits) :");
+		System.out.println("BIN key " + k.toString(2) + " t0 " + t0.toString(2) + " t1 " + t1.toString(2));
+		System.out.println("DEC key " + k.toString() + " t0 " + t0.toString() + " t1" + t1.toString());
+		System.out.println("HEX key " + k.toString(16) + " t0 " + t0.toString(16) + " t1 " + t0.toString(16) + "\n");
+		
+		
+		tab[0] = k.toString(2);
+		tab[1] = t0.toString(2);
+		tab[2] = t1.toString(2);
+		return tab;
+	}
+
+	public static void main(String[] args) {
+		KeyGenerator(256);
 	}
 }
