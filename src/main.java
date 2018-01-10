c26de4f219646c97d17652ac92db9e3f04dc0901
Benjamin <Benjamin@XPS11.home>
2018-01-10 14:41:29 +0100

    Prise en compte de n'importe quel fichier (avant merge)

--- src/main.java
+++ src/main.java
@@ -6,21 +6,6 @@ public class main {
 	public static void main(String[] args) {				
 		Scanner scanner = new Scanner(System.in);
 		
-		// choix de la méthode de chiffrement
-		System.out.println("\nSélectionner l'action souhaitée :");
-		System.out.println("1: Chiffrement symétrique ThreeFish");
-		System.out.println("2: Chiffrement de Cramer-Shoup");
-		System.out.println("3: Vérification d'un Hash");
-		System.out.println("4: Déchiffrement symétrique ThreeFish");
-		System.out.println("5: Déchiffrement Cramer-Shoup");
-		// On déclare la variable qui contient le menu
-		int menuInt = scanner.nextInt();
-		if (menuInt == 3) {
-			crypto.Hashage();
-			System.out.println("\nFin du programme.");
-			return;
-		}
-		
 		// Choix du fichier
 		System.out.println("Ecrivez le nom du fichier avec l'extension !");
 		String fileName = scanner.nextLine();
@@ -34,9 +19,24 @@ public class main {
 		
 		// On récupère le contenu du fichier
 		String fileContent = utils.ReadFile(fichier);
-		//System.out.println(fileContent);
+				//System.out.println(fileContent);
 		
-		System.out.println("Vous avez sélectionner le menu : " + menuInt);
+		// choix de la méthode de chiffrement
+		System.out.println("\nSélectionner l'action souhaitée :");
+		System.out.println("1: Chiffrement symétrique ThreeFish");
+		System.out.println("2: Chiffrement de Cramer-Shoup");
+		System.out.println("3: Vérification d'un Hash");
+		System.out.println("4: Déchiffrement symétrique ThreeFish");
+		System.out.println("5: Déchiffrement Cramer-Shoup");
+		// On déclare la variable qui contient le menu
+		int menuInt = scanner.nextInt();
+		if (menuInt == 3) {
+			crypto.Hashage();
+			System.out.println("\nFin du programme.");
+			return;
+		}		
+		
+		System.out.println("Vous avez sélectionné le menu : " + menuInt);
 		
 		// Variable qui contient le résultat de l'opération
 		String newContentFile = "";
